979e642fdbbfeebd6b36cbd1d0257ebde39a565f
Barometz-Mod <barometz.thorium@gmail.com>
2024-03-27 22:54:39 +0800

    rat (4)

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+function handleLoad() {
+    var spinner = document.getElementById("spinner");
+    var spinnerText = document.getElementById("spinner-loading");
+    var loadedText = document.getElementById("spinner-loaded");
+    setTimeout(function () {
+        spinner.remove();
+        spinnerText.remove();
+        loadedText.style.display = "block";
+    }, Math.floor(Math.random() * (3000 - 500 + 1) + 500));
+}
+
 function handleClick() {
-    document.getElementById("cover").remove();
-    document.getElementById("audio").play();
+    if (document.getElementById("spinner") != null) {
+        return;
+    } else {
+        document.getElementById("cover").remove();
+        document.getElementById("audio").play();
+    }
 }
